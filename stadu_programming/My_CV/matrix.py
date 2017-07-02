149b430ab05572e671f10ea166b332439403f19b
lliney <lliney1989@gmail.com>
2017-07-02 15:19:54 +0300

    python kurs#2+cv

---
+++ stadu_programming/My_CV/matrix.py
@@ -0,0 +1,18 @@
+import math
+import  numpy as np
+a=np.array([[1,0,0,0],
+            [0,1,0,0],
+            [0,0,1,0],
+            [0,0,0,1]])
+b=np.array([[1,2,3],
+            [5,6,7],
+            [9,10,11],
+            [4,8,12]])
+c=np.array([[3,0,0],
+            [0,3,0],
+            [0,0,3]])
+a=2*a
+k=np.dot(a,b)
+
+p=np.dot(k,c)
+print(p)
